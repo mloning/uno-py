b1039379fa340bcb60b8dce2be6fd0cd431220f6
mloning <21020482+mloning@users.noreply.github.com>
2023-12-12 00:03:06 -0500

    fix reverse, add tests, minor improvements

--- tests/test_players.py
+++ tests/test_players.py
@@ -2,12 +2,12 @@ from uno import Player, Players
 
 
 def _generate_test_players(n_players: int = 3) -> list[Player]:
-    return [Player(name=str(i)) for i in range(1, n_players + 1)]
+    players = [Player(name=str(i)) for i in range(1, n_players + 1)]
+    return Players(players)
 
 
 def test_players_next() -> None:
     players = _generate_test_players(n_players=3)
-    players = Players(players=players)
 
     assert players.next().name == "1"
     assert players.next().name == "2"
@@ -17,63 +17,64 @@ def test_players_next() -> None:
 
 def test_players_skip() -> None:
     players = _generate_test_players(n_players=3)
-    players = Players(players=players)
 
     assert players.next().name == "1"
+    players.skip()  # skipping player 2
+    assert players.next().name == "3"
+
+
+def test_players_skip_after_init() -> None:
+    players = _generate_test_players(n_players=3)
+
+    players.skip()  # skipping player 1
     assert players.next().name == "2"
     assert players.next().name == "3"
-    assert players.next().name == "1"
-    players.skip()  # skipping player B
-    assert players.next().name == "3"
 
 
-def test_players_double_reverse_idempotent() -> None:
-    players = _generate_test_players(n_players=4)
-    players = Players(players=players)
+def test_players_reverse_after_init() -> None:
+    players = _generate_test_players(n_players=3)
 
-    assert players.next().name == "1"
-    assert players.next().name == "2"
-    assert players.next().name == "3"
     players.reverse()  # reversing cycle
+    assert players.next().name == "3"
+    assert players.next().name == "2"
+    assert players.next().name == "1"
+    assert players.next().name == "3"
+
+
+def test_players_reverse_next_reverse() -> None:
+    players = _generate_test_players(n_players=4)
+
+    assert players.next().name == "1"
+    assert players.next().name == "2"
+    players.reverse()  # reversing cycle
+    assert players.next().name == "1"
+    players.reverse()  # reversing cycle
+    assert players.next().name == "2"
+    assert players.next().name == "3"
+    assert players.next().name == "4"
+
+
+def test_players_reverse_next_next_reverse() -> None:
+    players = _generate_test_players(n_players=4)
+
+    assert players.next().name == "1"
+    assert players.next().name == "2"
+    players.reverse()  # reversing cycle
+    assert players.next().name == "1"
+    assert players.next().name == "4"
+    players.reverse()  # reversing cycle
+    assert players.next().name == "1"
+    assert players.next().name == "2"
+    assert players.next().name == "3"
+
+
+def test_players_reverse_next_skip() -> None:
+    players = _generate_test_players(n_players=3)
+
+    assert players.next().name == "1"
+    assert players.next().name == "2"
     players.reverse()
-    assert players.next().name == "4"
     assert players.next().name == "1"
-    assert players.next().name == "2"
-
-
-def test_players_reverse_4_players() -> None:
-    players = _generate_test_players(n_players=4)
-    players = Players(players=players)
-
-    assert players.next().name == "1"
-    assert players.next().name == "2"
-    players.reverse()  # reversing cycle
-    assert players.next().name == "1"
-    assert players.next().name == "4"
-    assert players.next().name == "3"
-    assert players.next().name == "2"
-    players.reverse()  # reversing cycle
-    assert players.next().name == "3"
-    assert players.next().name == "4"
-    assert players.next().name == "1"
-    assert players.next().name == "2"
-
-
-def test_players_reverse_5_players() -> None:
-    players = _generate_test_players(n_players=5)
-    players = Players(players=players)
-
-    assert players.next().name == "1"
-    assert players.next().name == "2"
-    assert players.next().name == "3"
-    assert players.next().name == "4"
-    players.reverse()  # reversing cycle
-    assert players.next().name == "3"
+    players.skip()  # skipping player 3
     assert players.next().name == "2"
     assert players.next().name == "1"
-    assert players.next().name == "5"
-    assert players.next().name == "4"
-    players.reverse()  # reversing cycle
-    assert players.next().name == "5"
-    assert players.next().name == "1"
-    assert players.next().name == "2"
